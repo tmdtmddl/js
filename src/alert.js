3b4da33240f073962485d20637524bc26af54ddf
tmdtmddl <tmddlqkqh@naver.com>
2024-12-17 16:54:11 +0900

    commit message

---
+++ src/alert.js
@@ -0,0 +1,21 @@
+const fn = () => {
+  //   alert("Hellow");
+  // const ask =confirm("진짜??");
+  // if (ask) {
+  // alert{"ㅇㅋ"};
+  // else{
+  //     alert("ㄴㄴ");
+  // }
+  // }
+  const isChinkenGood = true;
+  if (!isChinkenGood) {
+    alert("치킨좋아");
+  } else {
+    alert("치킨살짐");
+  }
+};
+
+// 만들고 나서 내보내기!
+// const=>export 함수명
+
+export { fn };
